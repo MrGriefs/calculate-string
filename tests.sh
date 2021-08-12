50da2aeb698797659f1c0440b13dae84e06a3908
Aaron <trollololaaa@gmail.com>
2021-08-12 16:51:39 +0100

    tests: update for travis

--- tests.sh
+++ tests.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 
-. ~/.nvm/nvm.sh
+if ! errmsg=$(nvm); then . ~/.nvm/nvm.sh; fi
 
-echo "Building with webpack"
+echo "Building with webpack."
 if ! errmsg=$(nvm exec 16 npm run build 1>&2); then exit 1; fi
 
-echo "Test on latest node version"
-if ! errmsg=$(nvm exec 16 node tests 1>&2); then exit 1; fi
-
-echo "Test on minimum node version"
-if ! errmsg=$(nvm exec 0 node tests 1>&2); then exit 1; fi
+echo "Test on node $TRAVIS_NODE_VERSION."
+if ! errmsg=$(node tests 1>&2); then exit 1; fi
 
 exit 0
