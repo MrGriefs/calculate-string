6c0009d8c073c52c7545a4d2d0c4805c18cbbbd0
Aaron <trollololaaa@gmail.com>
2021-08-12 17:29:58 +0100

    tests: use exec in favour of node to specify version

--- tests.sh
+++ tests.sh
@@ -5,7 +5,7 @@ if ! errmsg=$(nvm); then . ~/.nvm/nvm.sh; fi
 echo "Building with webpack."
 if ! errmsg=$(nvm exec 16 npm run build 1>&2); then exit 1; fi
 
-echo "Test on node $TRAVIS_NODE_VERSION."
-if ! errmsg=$(node tests 1>&2); then exit 1; fi
+echo "Testing on node v$TRAVIS_NODE_VERSION."
+if ! errmsg=$(nvm exec $TRAVIS_NODE_VERSION node tests 1>&2); then exit 1; fi
 
 exit 0
